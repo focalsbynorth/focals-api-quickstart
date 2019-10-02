e05e824058f629355461e4eabd88a53e6837eae8
Nick Goral <32797167+nickgoral2@users.noreply.github.com>
2019-10-02 15:06:31 -0400

    Initial commit (#1)

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const Ability = require('@bynorth/focals-client-js');
+const config = require('nconf');
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const configPath = __dirname;
+
+config
+    .use('memory')
+    .argv()
+    .env({ lowerCase: true, separator: '__' })
+    .file('default', { file: `${configPath}/default.json` });
+
+
+Ability.init(config.get('quickstart'));
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/', require('./routes'));
+
+const port = config.get('port');
+app.listen(port, () => console.log(`HTTP server on!${port}`));
