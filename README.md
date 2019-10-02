b79a79370491d732351b263c86f817e2dd83a441
Nick Goral <nick.goral@thalmic.com>
2019-10-02 14:56:19 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# focals-api-quickstart
-Starter project to get developers up and running with the Focals Developer API **THIS IS AN ALPHA RELEASE**
+# Focals Developer API Quickstart
+
+This repository is intended to be used as a quickstart to show what's needed to create an ability for the Focals Developer API for Focals by North.  
+The initial example is provided only in NodeJS and is a barebones implementation - so things such as user management, caching, or a full implementation of features isn't provided.  
+
+## Components
+
+### index.js
+This is the entrypoint of the application.  
+In here we set up reading the config file, initialize the `focals-client-js` package, and set up the [Express](https://expressjs.com/) routes and starts the web server.  
+The way config is handled, or the web framework used, are optional - these are just set up to provide an example.
+
+### routes.js
+This is where the endpoints are defined that were applied in the above `index.js` file.  
+We have 3 routes configured in this quickstart example:
+1. `POST /trigger`
+    - This endpoint is used to send a packet to all users that have enabled your ability on Focals. In this example, we send a secure packet using end-to-end encryption by first retrieving the user's public keys, using the keys to encrypt the packet, and then posting the encrypted packet to the North endpoint.
+    - **NOTE** - The packet provided is not complete, and will want to be modified for your ability.
+2. `GET  /enable`
+    - This endpoint is used to allow users to enable your ability. This is the endpoint that will be configured as the `Enable URL` on the [Developer Portal](https://developer.bynorth.com) for your ability.
+    - The `focals-client-js` package is used to verify the received signature, and as this is just an example we perform no further checks and simply allow the user to enable the ability.
+    - **NOTE** - No storage or caching mechanism is in place in this quickstart, users are simply stored in a dictionary in memory. For a production ready ability, you'll want to implement a method of storing users however you want to.
+3. `POST /action`
+    - This endpoint is used to handle actions received from the Focals Developer API relating to a user. This is the endpoint that will be configured as the `Webhook URL` on the [Developer Portal](https://developer.bynorth.com) for your ability.
+    - **NOTE** - The only actions configured in this quickstart example are `integration:validate` and `integration:disable` which are used for handling the user enable and disable flows.
+
+### default.json
+This is an example configuration file which contains the `API Key`, `API Secret`, `Shared Secret` and `Integration ID` for your ability.  
+**NOTE** - These have been left blank and will need to be configured with values for your ability. These get generated for you when you first create your ability on the portal.  
+An alternative is to populating the file is to use environment variables to set the values. This can be done by setting environment variables with the `quickstart` prefix, followed by two underscores and the name of the variable. For example, `sharedSecret` would be `quickstart__sharedSecret`.
+
+## Dependencies
+- `@bynorth/focals-client-js`
+    - This is the library published by North to help ease the development of Abilities. This provides useful functionality to help implement end-to-end encryption, to verify received HMAC signatures, to retrieve a user's public keys for their Focals, and to build the enable URL.
+- `nconf`
+    - This package has been added to handle the configuration for the application using environment variables or a provided config file.
+- `request-promise-native`
+    - This package has been added to handle network requests so that the ability can send requests to the Focals Developer API.
+
+None of these libraries are mandatory - you can implement an ability any way you see fit (and in any language). We hope you find the `focals-client-js` package to be of use in making the development process easier.
---
+++ routes.js
@@ -0,0 +1,174 @@
+'use strict';
+
+const request = require('request-promise-native');
+const AbilitiesLibrary = require('@bynorth/focals-client-js');
+const config = require('nconf').get('quickstart');
+const express = require('express');
+
+const TOLERANCE_IN_SECONDS = 6 * 60;
+
+const router = express.Router();
+
+// For this quickstart example we're simply storing the enabled user information in a dictionary
+// You'll want to implement some form of data storage to track these users
+const usersEnabled = {};
+const usersUnvalidated = {};
+
+// This is an example packet, not fully populated
+const packet = {
+    packetId: 'north-quickstart-ability',
+    timestamp: '',
+    icon: {
+        type: 'URL',
+        value: 'https://via.placeholder.com/300'
+    },
+    title: 'North Quickstart',
+    actions:
+    [
+        {
+            type: 'system:reply',
+            actionId: 'reply',
+            title: 'Respond',
+        },
+        {
+            type: 'system:webhook',
+            title: 'Mark as Read',
+            actionId: 'mark_as_read',
+            icon: {
+                type: 'URL',
+                value: 'static:/system/icon/mark-as-read',
+            },
+        },
+    ],
+    body: 'Test quickstart message',
+    templateId: 'actionable_text',
+};
+
+/**
+ * Triggers a notification to all users that have your ability enabled on Focals, using end-to-end encryption.
+ *
+ * @param {string} req.query.sharedSecret - The shared secret to validate against to ensure the request came from your ability.
+ *
+ * @returns 200 OK  - If the actions were sent successfully to users.
+ *
+ * @throws Will throw an error if there's an issue retrieving public keys, encrypting the packet, or publishing to a user.
+ */
+router.post('/trigger', async (req, res) => {
+    // Validate shared secret
+    if (req.query.sharedSecret !== config.sharedSecret) {
+        return res.sendStatus(401);
+    }
+    console.log(`about to send to ${JSON.stringify(usersEnabled)}`);
+    for (const userId in usersEnabled) {
+        try {
+            console.log(`sending to ${userId}`);
+            const publicKeys = await AbilitiesLibrary.DeviceKeysService.getPublicKeys(userId, config.integrationId);
+            const pathsToEncrypt = [
+                '/packetId',
+                '/icon/value'
+            ];
+            console.log(`plain packet: ${JSON.stringify(packet)}`);
+            const encryptedPacket = await AbilitiesLibrary.EncryptionService.encryptPacket(packet, pathsToEncrypt, publicKeys);
+            console.log(JSON.stringify(encryptedPacket));
+            // Publish secure message
+            await request({
+                method: 'POST',
+                uri: 'https://cloud.bynorth.com/v1/api/integration/secure/publish-to-user',
+                body: {
+                    apiKey: config.apiKey,
+                    apiSecret: config.apiSecret,
+                    integrationId: config.integrationId,
+                    targetUserId: userId,
+                    packet: encryptedPacket
+                },
+                json: true
+            });
+        } catch (error) {
+            throw new Error(`Error publishing secure packet: ${error.message}`);
+        }
+    }
+
+    return res.sendStatus(200);
+});
+
+/**
+ * Enable endpoint for the ability.
+ * Validates the received signature, and if valid redirects to the North enable endpoint to confirm enabling your ability for the user.
+ *
+ * @param req.query.signature   - The HMAC signature for the request for validity.
+ * @param req.query.state       - The unique state for the enable process for the user.
+ * @param req.query.timestamp   - Timestamp of when the signature was generated and the request was sent.
+ *
+ * @returns 302 Redirect - Redirects to the North enable endpoint, including any errors that were encountered.
+ */
+router.get('/enable', async (req, res) => {
+    const { signature, state, timestamp } = req.query;
+
+    if (!AbilitiesLibrary.SignatureService.verifySignature(state, timestamp, signature)) {
+        return res.redirect(AbilitiesLibrary.UrlService.buildEnableUrl('', 'invalid_state'));
+    }
+
+    usersUnvalidated[state] = Date.now();
+
+    return res.redirect(AbilitiesLibrary.UrlService.buildEnableUrl(state));
+});
+
+/**
+ * Webhook endpoint for the ability.
+ * This endpoint is used to handle any incoming actions from North's Abilities Framework'.
+ * 
+ * @param req.query.sharedSecret - A shared secret that should match your ability's shared secret.
+ * @param req.body.type          - The type of action received.
+ * @param req.body.body.userId   - The userId relating to the action.
+ * @param req.body.body.state    - The unique state for the enable process for the user.
+ *
+ * @returns 200 OK                      - If the action was handled successfully.
+ * @returns 400 Bad Request             - If the received action type was unrecognised, or if the timestamp was outside the tolerance range.
+ * @returns 500 Internal Server Error   - An unhandled exception was thrown.
+ *
+ * @throw Throws an error if the shared secret is missing or invalid.
+ */
+router.post('/action', async (req, res) => {
+    const { sharedSecret } = req.query;
+
+    if (!sharedSecret) {
+        throw new Error('Did not provide shared secret');
+    }
+
+    if (sharedSecret !== config.sharedSecret) {
+        throw new Error('Invalid Shared Secret');
+    }
+
+    try {
+        console.log(`got an action ${JSON.stringify(req.body)}`);
+        const { type, body } = req.body;
+
+        if (type === 'integration:validate') {
+            const { state, userId } = body;
+
+            const userUnvalidatedTimestamp = usersUnvalidated[state] || 0;
+
+            if (Math.abs(Date.now() - userUnvalidatedTimestamp) > TOLERANCE_IN_SECONDS) {
+                return res.sendStatus(400);
+            }
+
+            usersEnabled[userId] = true;
+            delete usersUnvalidated[state];
+
+            return res.sendStatus(200);
+        }
+
+        if (type === 'integration:disable') {
+            delete usersEnabled[body.userId];
+            return res.sendStatus(200);
+        }
+
+        console.log(`unrecognized action: ${JSON.stringify(req.body)}`);
+
+        return res.sendStatus(400);
+    } catch (error) {
+        return res.sendStatus(500);
+    }
+});
+
+module.exports = router;
